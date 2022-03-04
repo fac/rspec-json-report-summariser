c376f1f415acf1e87acf5a5a83351e79fbb3403a
Peter Singh <peter.singh@freeagent.com>
2022-03-04 15:23:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const json_spec_reporter_summariser = require("./lib/json_spec_reporter_summariser.js");
+
+try {
+  const file_globs = core.getMultilineInput("file_globs");
+  const summary = json_spec_reporter_summariser(file_globs);
+
+  core.setOutput("duration", summary.summary.duration)
+  core.setOutput("example_count", summary.summary.example_count)
+  core.setOutput("failure_count", summary.summary.failure_count)
+  core.setOutput("pending_count", summary.summary.pending_count)
+  core.setOutput("errors_outside_of_examples_count", summary.summary.errors_outside_of_examples_count)
+
+  core.setOutput("summary_string", JSON.stringify(summary))
+} catch (error) {
+  core.setFailed(error.message);
+}
