eacbacaa5198b466eaac75d7b6ab1e7f6b2a87f2
Peter Singh <peter.singh@freeagent.com>
2022-03-08 10:21:51 +0000

    test release

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+
 const json_spec_reporter_summariser = require("./lib/json_spec_reporter_summariser.js");
 
 try {
